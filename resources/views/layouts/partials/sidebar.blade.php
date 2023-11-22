685db3c6e82fba96d51b64bc307277aeed21f702
toniadev <odubiyiifeolu@gmail.com>
2023-11-22 11:24:13 +0100

    inventory management set-up

---
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -0,0 +1,75 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4 bg-primary">
+    <!-- Brand Logo -->
+    <a href="{{url('/')}}" class="brand-link">
+        <img src="{{ asset('images/logo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+             style="opacity: .8">
+        <span class="brand-text font-weight-light">The Cashew Venture</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <!-- Sidebar user (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+                <img src="" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+                <a href="#" class="d-block">Tonia</a>
+            </div>
+        </div>
+
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <li class="nav-item has-treeview">
+                    <a href="{{url('/')}}" class="nav-link">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>Dashboard</p>
+                    </a>
+                </li>
+                <li class="nav-item has-treeview">
+                    <a href="{{ url('/products') }}" class="nav-link">
+                        <i class="nav-icon fas fa-th-large"></i>
+                        <p>Products</p>
+                    </a>
+                </li>
+                <li class="nav-item has-treeview">
+                    <a href="{{ url('/cart') }}" class="nav-link">
+                        <i class="nav-icon fas fa-cart-plus"></i>
+                        <p>Open POS</p>
+                    </a>
+                </li>
+                <li class="nav-item has-treeview">
+                    <a href="{{ url('/orders') }}" class="nav-link">
+                        <i class="nav-icon fas fa-cart-plus"></i>
+                        <p>Orders</p>
+                    </a>
+                </li>
+                <li class="nav-item has-treeview">
+                    <a href="{{ url('/customers') }}" class="nav-link">
+                        <i class="nav-icon fas fa-users"></i>
+                        <p>Customers</p>
+                    </a>
+                </li>
+                <li class="nav-item has-treeview">
+                    <a href="{{ url('/settings') }}" class="nav-link }}">
+                        <i class="nav-icon fas fa-cogs"></i>
+                        <p>Settings</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link" onclick="document.getElementById('logout-form').submit()">
+                        <i class="nav-icon fas fa-sign-out-alt"></i>
+                        <p>Logout</p>
+                        <form action="{{url('logout')}}" method="POST" id="logout-form">
+                            @csrf
+                        </form>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
